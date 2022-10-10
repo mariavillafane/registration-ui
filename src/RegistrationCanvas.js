4b338fcb7bed01a9c170632b2fd60c5a052a0d50
Villafane <mv1419@ic.ac.uk>
2022-10-10 16:53:56 +0100

    clip-path WIP

--- src/RegistrationCanvas.js
+++ src/RegistrationCanvas.js
@@ -2,19 +2,19 @@ import { useState, useRef } from "react";
 
 //ZOOM
 function calculateViewBoxChange(event, viewBox, ref) {
-  //console.log(event);  //console.log(event.target.getBoundingClientRect());
-  console.log(ref.current.getBoundingClientRect()); //the svg
-
+  ////console.log(event);  //console.log(event.target.getBoundingClientRect());
+  //console.log(ref.current.getBoundingClientRect()); //the svg
+  //console.log(event.clientX)
   const mouseX = -ref.current.getBoundingClientRect().x + event.clientX;
-  console.log(mouseX);
+  //console.log(mouseX);
 
   const mouseY = -ref.current.getBoundingClientRect().y + event.clientY;
-  console.log(mouseY);
+  //console.log(mouseY);
 
   const canvasWidth = +ref.current.clientWidth;
   const canvasHeight = +ref.current.clientHeight;
 
-  console.log(ref.current);
+  //console.log(ref.current);
 
   const mouseScroll = event.deltaY > 0 ? 1 : -1; //if event bigger than 0, then 1, else -1
 
@@ -24,7 +24,7 @@ function calculateViewBoxChange(event, viewBox, ref) {
   const offsetX = (mouseX / canvasWidth) * (viewBox[2] - newViewBoxWidth);
   const offsetY = (mouseY / canvasHeight) * (viewBox[3] - newViewBoxHeight);
 
-  console.log({ offsetX, offsetY, mx: mouseX / canvasWidth });
+  //console.log({ offsetX, offsetY, mx: mouseX / canvasWidth });
 
   const newViewBox = [
     viewBox[0] + offsetX,
@@ -90,7 +90,9 @@ export function RegistrationCanvas(props) {
       onWheel={(event) =>
         setViewBox(calculateViewBoxChange(event, viewBox, ref))
       }
+      
       {...handleDrag(ref, mousePosition, setMousePosition, setViewBox, viewBox)}
+
       viewBox={viewBox.join(" ")}
       //viewBox={`0 0 200 200`}   //ZOOM
       //preserveAspectRatio="xMinYMin meet" //"none"//xMinYMin slice" //https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox
@@ -99,17 +101,48 @@ export function RegistrationCanvas(props) {
         height: "600px", // pixels on the screen
       }}
     >
-      <image id="myFixedImage" {...props.fixed} />
-      <image id="myMovingImage" {...props.moving} />
-      <rect //https://developer.mozilla.org/en-US/docs/Web/SVG/Element/clipPath
-        id="myWorkSpace"
-        x={0}
-        y={0}
-        width={props.canvas_X}
-        height={props.canvas_Y}
-        fill="none"
-        stroke="green"
-      />
+
+    <image id="myFixedImage" {...props.fixed} />
+    <image id="myMovingImage" {...props.moving} />
+    <rect //https://developer.mozilla.org/en-US/docs/Web/SVG/Element/clipPath
+      id="myWorkSpace"
+      x={0}
+      y={0}
+      width={props.canvas_X}
+      height={props.canvas_Y}
+      fill="none"
+      stroke="green"
+    />
+    <text x={0} y={0} fill="green"> 0 </text>
+
+    
+      {/*  
+      <clipPath id="myClip" >
+        <rect //https://developer.mozilla.org/en-US/docs/Web/SVG/Element/clipPath
+          id="myWorkSpace"
+          x={0}
+          y={0}
+          width={props.canvas_X}
+          height={props.canvas_Y}
+          fill="none"
+          stroke="green"
+        />
+      </clipPath>
+      
+      <image id="myFixedImage" {...props.fixed} clip-path="url(#myClip)" />
+      <image id="myMovingImage" {...props.moving} clip-path="url(#myClip)" />
+
+    
+      https://developer.mozilla.org/en-US/docs/Web/CSS/clip-path
+      https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/clip-path
+      https://developer.mozilla.org/en-US/docs/Web/SVG/Element/clipPath
+
+*/
+    }
+
+    
+      
+      
     </svg>
     //</div>
   );
